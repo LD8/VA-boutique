69c69127c46894da4bc4302caf45370a8af185ac
LD8 <don_lee@me.com>
2020-02-03 20:59:39 +0800

    landing page 70%

--- boutique/models.py
+++ boutique/models.py
@@ -8,29 +8,50 @@ class Category(models.Model):
     women = models.BooleanField()
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300, blank=True)
-    uploaded_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    uploaded_date = models.DateTimeField(
+        auto_now_add=True, null=True, blank=True)
+
+    class Meta():
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return ("Men's " + self.name) if self.men else ("Women's " + self.name)
+
 
 class SubCategory(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300, blank=True)
-    uploaded_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    uploaded_date = models.DateTimeField(
+        auto_now_add=True, null=True, blank=True)
+
+    class Meta():
+        verbose_name = 'Sub-Category'
+        verbose_name_plural = 'Sub-Categories'
 
     def __str__(self):
         return ("Men's " + self.name) if self.category.men else ("Women's " + self.name)
 
+
 class Item(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, blank=True)
+    subcategory = models.ForeignKey(
+        SubCategory, on_delete=models.CASCADE, blank=True)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300, blank=True)
     price = models.FloatField(default='0')
-    discount = models.IntegerField(default='0')
-    uploaded_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    image = models.ImageField(upload_to='items/')
+    discount = models.IntegerField(null=True, blank=True)
+    uploaded_date = models.DateTimeField(
+        auto_now_add=True, null=True, blank=True)
+
+    image_1 = models.ImageField(upload_to='items/')
+    image_2 = models.ImageField(upload_to='items/', blank=True)
+    image_3 = models.ImageField(upload_to='items/', blank=True)
+    image_4 = models.ImageField(upload_to='items/', blank=True)
+    image_5 = models.ImageField(upload_to='items/', blank=True)
+
+    class Meta:
+        ordering = ['-uploaded_date']
 
     def __str__(self):
         return self.name
