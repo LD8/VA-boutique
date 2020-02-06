ab676b3b2b5a5759364308b70747580e955511df
LD8 <don_lee@me.com>
2020-02-06 23:59:32 +0800

    today

--- boutique/urls.py
+++ boutique/urls.py
@@ -3,9 +3,18 @@ from . import views
 
 app_name = 'boutique'
 urlpatterns = [
-    # path('', views.index, name='index'),
+    # show index page
     path('', views.IndexView.as_view(), name='index'),
-    path('women/', views.ItemListView.as_view(template_name = 'boutique/items.html'), name='women-all'),
-    path('men/', views.ItemListView.as_view(template_name = 'boutique/items.html'), name='men-all'),
 
+    # show categories of products for men or women
+    path('<slug:gender>/', views.ItemListView.as_view(), name='show-all'),
+
+    # show a specific category for men or women
+    path('<slug:gender>/cat_<int:category_pk>/', views.ItemListView.as_view(), name='category'),
+
+    # show a specific subcategory under a specific category for men or women
+    path('<slug:gender>/cat_<int:category_pk>/subcat_<int:subcategory_pk>/', views.ItemListView.as_view(), name='subcategory'),
+
+    # show a specific item
+    path('item_<int:item_pk>/', views.ItemDetailView.as_view(), name='item'),
 ]
--- boutique/views.py
+++ boutique/views.py
@@ -1,29 +1,45 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from .models import Category, Item
-from django.views.generic.base import TemplateView
-from django.views.generic.list import ListView
-from .models import Category
-
-# def index(request):
-#     '''landing page'''
-#     categories = Category.objects.all()
-#     context = {'categories':categories}
-#     return render(request, 'boutique/index.html', context)
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     '''landing page'''
+    model = Category
     template_name = 'boutique/index.html'
+    context_object_name = 'categories'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['categories'] = Category.objects.all()
-        return context
 
 class ItemListView(ListView):
     '''item page'''
-    model = Category
-    # template_name = 'boutique/items.html'
+    # model = Category
+    template_name = 'boutique/items.html'
+    context_object_name = 'categories'
+    paginate_by = 12
+
+    def get_object(self):
+        # obj = super().get_object()
+        obj = get_object_or_404(Category, pk=self.kwargs.get('category_pk'))
+        return obj
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if 'women' in self.request.GET:
+            context['categories'] = Category.objects.filter(women=True)
+        elif 'men' in self.request.GET:
+            context['categories'] = Category.objects.filter(men=True)
+        if 'category_pk' in self.request.GET:
+            
+        return context
+
+class ItemDetailView(DetailView):
+    '''item page'''
+    # model = Item
+    template_name = 'boutique/item.html'
+    context_object_name = 'item'
+
+    def get_object(self):
+        return get_object_or_404(Item, pk=self.kwargs['item_pk'])
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
