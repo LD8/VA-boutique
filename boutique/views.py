bb578524f5ea0d940f2ad0008615eee582ac8700
LD8 <don_lee@me.com>
2020-02-07 11:31:28 +0800

    before changing men/women selection

--- boutique/views.py
+++ boutique/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
-from .models import Category, Item
+from .models import Category, Item, SubCategory
 
 
 class IndexView(ListView):
@@ -11,37 +11,64 @@ class IndexView(ListView):
 
 
 class ItemListView(ListView):
-    '''item page'''
-    # model = Category
+    '''display a list of items'''
+    model = Item
     template_name = 'boutique/items.html'
-    context_object_name = 'categories'
-    paginate_by = 12
+    # paginate_by = 12
 
-    def get_object(self):
-        # obj = super().get_object()
-        obj = get_object_or_404(Category, pk=self.kwargs.get('category_pk'))
-        return obj
+    def get_queryset(self):
+        # get original queryset: Item.objects.all()
+        qs = super().get_queryset()
+
+        # filter items: men/women
+        if self.kwargs['gender'] == 'women':
+            qs = qs.filter(category__women=True)
+        elif self.kwargs['gender'] == 'men':
+            qs = qs.filter(category__men=True)
+
+        if self.kwargs.get('category_pk'):
+            qs = qs.filter(category=self.kwargs.get('category_pk'))
+            if self.kwargs.get('subcategory_pk'):
+                qs = qs.filter(subcategory=self.kwargs.get('subcategory_pk'))
+
+        # print(qs)
+        return qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        if 'women' in self.request.GET:
-            context['categories'] = Category.objects.filter(women=True)
-        elif 'men' in self.request.GET:
-            context['categories'] = Category.objects.filter(men=True)
-        if 'category_pk' in self.request.GET:
-            
+        # add categories for navbar link texts
+        context['categories'] = Category.objects.all()
+
+        if self.kwargs.get('gender') == 'women':
+            context['category_shown'] = Category.objects.filter(women=True)
+        if self.kwargs.get('gender') == 'men':
+            context['category_shown'] = Category.objects.filter(men=True)
+
+        if self.kwargs.get('category_pk'):
+            context['category_shown']=get_object_or_404(Category, pk=self.kwargs.get('category_pk'))
+            if self.kwargs.get('subcategory_pk'):
+                context['subcategory_shown']=get_object_or_404(SubCategory, pk=self.kwargs.get('subcategory_pk'))
+
+        # print(context)
         return context
 
+
 class ItemDetailView(DetailView):
-    '''item page'''
-    # model = Item
+    '''display an individual item'''
+    model = Item
     template_name = 'boutique/item.html'
     context_object_name = 'item'
 
     def get_object(self):
-        return get_object_or_404(Item, pk=self.kwargs['item_pk'])
+        obj = super().get_object()
+        obj = get_object_or_404(Item, pk=self.kwargs.get('item_pk'))
+        print(obj)
+        return obj
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['categories'] = Category.objects.all()
+        # add categories for navbar link texts
+        # context['categories'] = Category.objects.all()
+        print('123')
+        print(context)
         return context
