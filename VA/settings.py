7a9a6c52154d0fb263969180bcc4192c6ed0d72e
LD8 <don_lee@me.com>
2020-03-01 19:21:09 +0800

    add .mo file and djust settings, debug false

--- VA/settings.py
+++ VA/settings.py
@@ -13,7 +13,7 @@ if DEBUG:
 # production setting
 else:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    ALLOWED_HOSTS = ['va-boutique.com', '5.63.152.4', 'localhost']
+    ALLOWED_HOSTS = ['va-boutique.com', 'www.va-boutique.com', '5.63.152.4', 'localhost']
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
@@ -129,18 +129,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-# LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'ru'
+if DEBUG:
+    LANGUAGE_CODE = 'en-us'
+else:
+    LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
-TIME_ZONE = 'Europe/Moscow'
 
 LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
