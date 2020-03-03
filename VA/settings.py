9ec043e7e3d48ea29e9872ecf4c1799932dd5555
LD8 <don_lee@me.com>
2020-03-03 13:12:16 +0800

    DATABASE SINGULIIZE

--- VA/settings.py
+++ VA/settings.py
@@ -92,24 +92,24 @@ WSGI_APPLICATION = 'VA.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-if not os.environ.get('USE_PROD_DB', None):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+# if not os.environ.get('USE_PROD_DB', None):
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'va',
+        'USER': 'va_db_admin',
+        'PASSWORD': os.environ.get('DB_PASSWD'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'va',
-            'USER': 'va_db_admin',
-            'PASSWORD': os.environ.get('DB_PASSWD'),
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
+}
 
 
 # Password validation
