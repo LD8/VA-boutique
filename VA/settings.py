68789b82421d7ca60d6940c0303a861ddcee2001
LD8 <don_lee@me.com>
2020-03-05 20:27:58 +0800

    back to normal setting

--- VA/settings.py
+++ VA/settings.py
@@ -96,31 +96,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'VA.wsgi.application'
 
-DATABASES = {
+# Database settings
+if not os.environ.get('USE_PROD_DB', None):
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-# # Database settings
-# if not os.environ.get('USE_PROD_DB', None):
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'vadb',
-#             'USER': 'va_db_admin',
-#             'PASSWORD': os.environ.get('DB_PASSWD'),
-#             'HOST': 'localhost',
-#             'PORT': '',
-#         }
-#     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'vadb',
+            'USER': 'va_db_admin',
+            'PASSWORD': os.environ.get('DB_PASSWD'),
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
