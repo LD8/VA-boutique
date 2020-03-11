11e52a6c8a314c1716c784a4d2db22e588315704
LD8 <don_lee@me.com>
2020-03-11 13:04:25 +0800

    wow animate update

--- VA/settings.py
+++ VA/settings.py
@@ -2,7 +2,7 @@ from django.contrib.messages import constants as messages
 import os
 
 # always False when pushing to Git
-DEBUG = True
+DEBUG = False
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
